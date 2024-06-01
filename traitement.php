9512027ae2a27e7e92c445ab6d2dbafcbd765c2b
Casperdev4 <allam.bilal91@gmail.com>
2024-06-01 16:28:53 +0200

    fghj

--- traitement.php
+++ traitement.php
@@ -7,10 +7,10 @@ $telephone = htmlspecialchars($_POST['telephone'], ENT_QUOTES, 'UTF-8');
 $services = htmlspecialchars($_POST['services'], ENT_QUOTES, 'UTF-8');
 $commentaires = htmlspecialchars($_POST['commentaires'], ENT_QUOTES, 'UTF-8');
 
-$message = "NOM : $nom \n";
-$message .= "TELEPHONE : $telephone \n";
-$message .= "PRESTATION : $services \n";
-$message .= "COMMENTAIRES : $commentaires \n";
+$message = "Nom : $nom \n";
+$message .= "Téléphone : $telephone \n";
+$message .= "Prestation : $services \n";
+$message .= "Commentaires : $commentaires \n";
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
